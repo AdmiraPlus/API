781b65648f46a981ee2a5a21bb466a00ef83b96f
Leoncio R. Barrios H <leonciobarriosh@gmail.com>
2023-06-14 12:28:21 -0500

    La conexión y busqueda en la BD SQL Server OK

--- API/conexion_db.py
+++ API/conexion_db.py
@@ -1,28 +1,35 @@
 import pyodbc
 
+def get_conexion():
+	server_name = "LEONCIOPC"
+	db_name = "BICAMutual"
+	#username = "sa"
+	#password = "AdmiraPlus2015"
 
-server = 'LEONCIOPC'
-data_base = 'BICAMutual'
-user = 'sa'
-password = 'AdmiraPlus2015'
 
-print("Empezamos")
-
-try:
-	conexion = pyodbc.connect(
-		'Driver={ODBC Driver 17 for SQL Server};'
-		f'Server={server};'
-		f'Database={data_base};'
-		f'UID={user};'
-		f'PWD={password};'
-	)
-	
-	#    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-	#                              direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
-	
-	# OK! conexión exitosa
-	print("Conexión exitosa***!!!")
-	
-except Exception as e:
-	# Atrapar error
-	print("Ocurrió un error al conectar a SQL Server: ", e)
+	#print("Empezamos ***********")
+	conexion = None
+	try:
+		""" conexion = pyodbc.connect(
+			'Driver={ODBC Driver 17 for SQL Server};'
+			f'Server={server_name};'
+			f'Database={db_name};'
+			f'UID={username};'
+			f'PWD={password};'
+		)"""
+		
+		conexion = pyodbc.connect(
+			'Driver={ODBC Driver 17 for SQL Server};'
+			f'Server={server_name};'
+			f'Database={db_name};'
+			'Trusted_Connection=yes;'
+		)
+		
+		# OK! conexión exitosa
+		#print("Conexión exitosa***!!!")
+		#print(f'El obj conexión: {conexion}')
+		
+	except Exception as e:
+		# Atrapar error
+		print("Ocurrió un error al conectar a SQL Server: ")
+	return conexion
--- API/main.py
+++ API/main.py
@@ -5,21 +5,37 @@ from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
-from conexion_db import conexion
+#from conexion_db import conexion
+import conexion_db
 
+
+#-- Zona de pruebas -------------
+'''
+username = "leoncios"
 try:
-	with conexion.cursor() as cursor:
-		cursor.execute("select * from AMVSaldos")
-		
-		filas = cursor.fetchall()
-		
-		for f in filas:
-			print(f)
-
+	print("Va a obtener la conexión")
+	conexion = conexion_db.get_conexion()
+	print("Va a comprobar la conexión")
+	if conexion:
+		#print("entró a hacer la consulta")
+		with conexion.cursor() as cursor:
+			inst_sql = "SELECT username, password, client_id, client_secret FROM UserAPI WHERE username = ?"
+			cursor.execute(inst_sql, username)
+			
+			user_db = cursor.fetchone()
+			
+			print(user_db)
+	else:
+		print("No hay conexión")
 except Exception as	e:
-	print("Ocurrió un error al consultar: ", e)
+	print("Ocurrió un error al consultar: ")
 finally:
-    conexion.close()
+	if conexion:
+		conexion.close()
+
+'''
+#------------------------------------
+
 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_DURATION = 1
@@ -34,6 +50,8 @@ oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 crypt = CryptContext(schemes=["bcrypt"])
 
 
+#-- Modelos ---------------
+
 class User(BaseModel):
 	username: str
 	full_name: str
@@ -43,16 +61,28 @@ class User(BaseModel):
 class UserDB(User):
 	password: str
 
+	
+class UserAPI(BaseModel):
+	username: str
+	full_name: str
+	email: str
+	disabled: bool
+
+class UserAPIDB(UserAPI):
+	password: str
+	client_id: str
+	client_secret: str
+
 
 #-- Datos -----------------
 
-users_db = {
+x_users_db = {
 	"leoncio": {
 		"username": "leoncio",
 		"full_name": "Leoncio Barrios",
 		"email": "leonciobarriosh@gmail.com",
 		"disabled": False,
-		"password": "$2a$12$0EQkKNfgPq5hCTrNS2.TOeHK/y9pH13DhTB/q/tg6Pg96besfSnfS"  # 123456
+		"password": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"  # 123456
 	},
 	"fulano": {
 		"username": "fulano",
@@ -66,14 +96,64 @@ users_db = {
 
 # -- Funciones ----------------------------------------------
 
+def buscar_usuario_bd(username: str):
+	user_db = None
+	try:
+		conexion = conexion_db.get_conexion()
+		if conexion:
+			#print("entró a hacer la consulta")
+			with conexion.cursor() as cursor:
+				cursor.execute("SELECT * FROM UserAPI WHERE username = ?", username)
+				
+				user_db = cursor.fetchone()
+				
+				#print(user_db)
+		#else:
+		#	print("No hay conexión")
+	except Exception as	e:
+		print("Ocurrió un error al consultar: ")
+	finally:
+		if conexion:
+			conexion.close()
+	return user_db
+#----------------------------------------------------------------
+'''
 def search_user_db(username: str):
 	if username in users_db:
 		return UserDB(**users_db[username])
-
+'''
+def search_user_db(username: str):
+	user_db = buscar_usuario_bd(username)
+	if user_db:
+		#print(f"El usuario encontrado es: {user_db}")
+		
+		return UserAPIDB(
+			username=user_db[1],
+			full_name=user_db[2],
+			email=user_db[3],
+			disabled=user_db[4],
+			password=user_db[5],
+			client_id=user_db[6],
+			client_secret=user_db[7]
+		)
+ 		#return UserDB(**users_db[username])
+#----------------------------------------------------------------
+'''
 def search_user(username: str):
 	if username in users_db:
 		return User(**users_db[username])
+'''
+def search_user(username: str):
+	user = buscar_usuario_bd(username)
+	if user:
+		return UserAPI(
+			username=user[1],
+			full_name=user[2],
+			email=user[3],
+			disabled=user[4]
+		)
 
+#----------------------------------------------------------------
 async def auth_user(token: str = Depends(oauth2)):
 	exception = HTTPException(
 		status_code=status.HTTP_401_UNAUTHORIZED,
@@ -91,7 +171,7 @@ async def auth_user(token: str = Depends(oauth2)):
 	
 	return search_user(username)
 
-async def current_user(user: User = Depends(auth_user)):
+async def current_user(user: UserAPI = Depends(auth_user)):   # async def current_user(user: User = Depends(auth_user)):
 	if user.disabled:
 		raise HTTPException(
 			status_code=status.HTTP_400_BAD_REQUEST,
@@ -103,38 +183,37 @@ async def current_user(user: User = Depends(auth_user)):
 
 # -- EndPoints ---------------------------------------------
 
-@ app.post("/login")
+@app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-	user_db = users_db.get(form.username)
 	# -- Validar si existe el "username".
+	user_db = buscar_usuario_bd(form.username)   # user_db = users_db.get(form.username)
 	if not user_db:
 		raise HTTPException(
 			status_code=status.HTTP_400_BAD_REQUEST,
 			detail="El usuario no es correcto"
 		)
 	user = search_user_db(form.username)
-
+	
 	# -- Validar el password.
-	# if not form.password == user.password:
 	if not crypt.verify(form.password, user.password):
 		raise HTTPException(
 			status_code=status.HTTP_400_BAD_REQUEST,
 			detail="El password no es correcto"
 		)
-
+	
 	# expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
-
+	
 	access_token = {
 		"sub": user.username,
 		"exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
 	}
-
+	
 	return {
 		"access_token": jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM),
 		"token_type": "bearer"
 	}
 
 
-@ app.get("/users/me")
-async def me(user: User = Depends(current_user)):
+@app.get("/users/me")
+async def me(user: UserAPI = Depends(current_user)):   # async def me(user: User = Depends(current_user)):
 	return user
